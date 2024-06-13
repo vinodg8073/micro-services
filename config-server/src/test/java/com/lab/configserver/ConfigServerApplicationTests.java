7ae9df5dc6508cb6a37657867c55e2bc8e5da16c
Vinod G <98164064+vinodg8073@users.noreply.github.com>
2024-06-13 09:54:25 +0530

    Created SCCS to read configs from classpath

---
+++ config-server/src/test/java/com/lab/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
